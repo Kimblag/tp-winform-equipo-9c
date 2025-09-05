e28bf7dad489a326dc9fa1363f9144cd5663ba1b
Kimblag <kimberly.blandong@gmail.com>
2025-09-05 16:24:35 -0300

    Se agrega Archivo Formulario Principal para la ventana padre que mostrará las vistas para gestionar el catalogo

---
+++ CatalogoArticulos.UI/Formularios/Principal/FormPrincipal.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CatalogoArticulos.UI.Formularios.Principal
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnCategorias = new System.Windows.Forms.Button();
+            this.btnMarcas = new System.Windows.Forms.Button();
+            this.btnArticulos = new System.Windows.Forms.Button();
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelMenu.Controls.Add(this.btnCategorias);
+            this.panelMenu.Controls.Add(this.btnMarcas);
+            this.panelMenu.Controls.Add(this.btnArticulos);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(180, 588);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnCategorias
+            // 
+            this.btnCategorias.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCategorias.FlatAppearance.BorderSize = 0;
+            this.btnCategorias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategorias.Image = global::CatalogoArticulos.UI.Properties.Resources.iconCategoria;
+            this.btnCategorias.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCategorias.Location = new System.Drawing.Point(0, 80);
+            this.btnCategorias.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCategorias.Name = "btnCategorias";
+            this.btnCategorias.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnCategorias.Size = new System.Drawing.Size(180, 40);
+            this.btnCategorias.TabIndex = 2;
+            this.btnCategorias.Text = "Categorías";
+            this.btnCategorias.UseVisualStyleBackColor = true;
+            // 
+            // btnMarcas
+            // 
+            this.btnMarcas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMarcas.FlatAppearance.BorderSize = 0;
+            this.btnMarcas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMarcas.Image = global::CatalogoArticulos.UI.Properties.Resources.iconMarca;
+            this.btnMarcas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMarcas.Location = new System.Drawing.Point(0, 40);
+            this.btnMarcas.Margin = new System.Windows.Forms.Padding(5);
+            this.btnMarcas.Name = "btnMarcas";
+            this.btnMarcas.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnMarcas.Size = new System.Drawing.Size(180, 40);
+            this.btnMarcas.TabIndex = 1;
+            this.btnMarcas.Text = "Marcas";
+            this.btnMarcas.UseVisualStyleBackColor = true;
+            // 
+            // btnArticulos
+            // 
+            this.btnArticulos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnArticulos.FlatAppearance.BorderSize = 0;
+            this.btnArticulos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnArticulos.Image = global::CatalogoArticulos.UI.Properties.Resources.iconArticulos;
+            this.btnArticulos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnArticulos.Location = new System.Drawing.Point(0, 0);
+            this.btnArticulos.Margin = new System.Windows.Forms.Padding(5);
+            this.btnArticulos.Name = "btnArticulos";
+            this.btnArticulos.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnArticulos.Size = new System.Drawing.Size(180, 40);
+            this.btnArticulos.TabIndex = 0;
+            this.btnArticulos.Text = "Artículos";
+            this.btnArticulos.UseVisualStyleBackColor = true;
+            // 
+            // panelContenido
+            // 
+            this.panelContenido.BackColor = System.Drawing.Color.White;
+            this.panelContenido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenido.Location = new System.Drawing.Point(180, 0);
+            this.panelContenido.Name = "panelContenido";
+            this.panelContenido.Size = new System.Drawing.Size(753, 588);
+            this.panelContenido.TabIndex = 1;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(933, 588);
+            this.Controls.Add(this.panelContenido);
+            this.Controls.Add(this.panelMenu);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Catálogo de artículos";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnCategorias;
+        private System.Windows.Forms.Button btnMarcas;
+        private System.Windows.Forms.Button btnArticulos;
+        private System.Windows.Forms.Panel panelContenido;
+    }
+}
--- CatalogoArticulos.UI/Formularios/Principal/FormPrincipal.cs
+++ CatalogoArticulos.UI/Formularios/Principal/FormPrincipal.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CatalogoArticulos.UI
+namespace CatalogoArticulos.UI.Formularios.Principal
 {
-    public partial class Form1 : Form
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        public FormPrincipal()
         {
             InitializeComponent();
         }
