5e49c064196030e25dfd40acefccba773c367cc5
Kimblag <kimberly.blandong@gmail.com>
2025-09-06 20:59:25 -0300

    Se agrega clase AccesoDatos para centralizar conexión a BD

---
+++ CatalogoArticulos.Negocio/AccesoDatos.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CatalogoArticulos.Negocio
+{
+    internal class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+
+        public void DefinirConsulta(string consultaSql)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consultaSql;
+        }
+
+
+        public void EjecutarConsulta()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                comando.Connection.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+
+
+    }
+}
